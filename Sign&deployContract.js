612a6427b0191a3332d68dbdc04bb898c794f8d5
oo7ww <xlovebear@126.com>
2018-05-03 17:07:49 +0800

    signed undefined

--- Sign&deployContract.js
+++ Sign&deployContract.js
@@ -14,15 +14,15 @@ const contract = new web3.eth.Contract(abi);
 
 var firstAccount = web3.eth.accounts.create();
 const private_key0 = firstAccount.privateKey;
-
+console.log('This is private key: '  + private_key0);
 console.log('This is address: ' + firstAccount.address);
 console.log('This is bin: ' + bytecode);
-var tx = {data: '0x' + bytecode, gas: 1000000};
+var tx = {chainId: 10, data: '0x' + bytecode, gas: 1000000};
 
-var signed = web3.eth.accounts.signTransaction(tx, private_key0).then(console.log);
+var signed = web3.eth.accounts.signTransaction(tx, private_key0);
 
 console.log(signed.rawTransaction);
 
-var sendover = web3.eth.sendSignedTransaction(signed.rawTransaction);
+var sendover = web3.eth.sendSignedTransaction((signed.rawTransaction).toString('hex'));
 
 console.log('send finished');
