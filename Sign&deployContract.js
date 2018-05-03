109926df454b1f24415a3e4677382c47baf12c72
oo7ww <xlovebear@126.com>
2018-05-03 15:54:11 +0800

    Update Sign&deployContract.js

--- Sign&deployContract.js
+++ Sign&deployContract.js
@@ -15,8 +15,8 @@ const contract = new web3.eth.Contract(abi);
 var firstAccount = web3.eth.accounts.create();
 const private_key0 = firstAccount.privateKey;
 
-console.log(firstAccount.address);
-
+console.log('This is address: ' + firstAccount.address);
+console.log('This is bin: ' + bytecode);
 var tx = {data: '0x' + bytecode, gas: 1000000};
 
 var signed = web3.eth.accounts.signTransaction(tx, private_key0).then(console.log);
