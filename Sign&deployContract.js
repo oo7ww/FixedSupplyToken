94a46ea7f6dc61b7ab18c438f1dbbd145b515ff5
oo7ww <xlovebear@126.com>
2018-05-07 11:43:16 +0800

    Update Sign&deployContract.js

--- Sign&deployContract.js
+++ Sign&deployContract.js
@@ -18,7 +18,7 @@ const private_key0 = '0x4fdeceafa4352e5eef12a4d34bf5bc9459c85462f7d5ebe59e6555ff
 
 //console.log(firstAccount.address);
 
-var tx = {data: '0x' + bytecode, gas: 1000000};
+var tx = {data: '0x' + bytecode, gas: 10000000};
 
 //var signed = web3.eth.accounts.signTransaction(tx, private_key0).then(console.log);
 //web3.eth.sendSignedTransaction(signed.rawTransaction).then(console.log);
