24e03883d2e8f4e212c82f81eca7ea2e39631e42
oo7ww <xlovebear@126.com>
2018-05-07 11:25:04 +0800

    Add files via upload

---
+++ newAccount.js
@@ -0,0 +1,19 @@
+//account new&store
+const Web3 = require('web3');
+const fs = require('fs');
+
+const web3 = new Web3('http://localhost:8381');
+
+var newAccount = web3.eth.account.create();
+
+const address = newAccount.address;
+const privateKey = newAccount.privateKey;
+
+fs.writeFile('coinbase.txt',privateKey, function(err){
+    if(err){
+        return console.log(err);
+    }
+    console.log('keystore successed');    
+}
+);
+
