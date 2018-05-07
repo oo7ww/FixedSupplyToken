d99d0ddea197713841a5d02ef152413099a87812
oo7ww <xlovebear@126.com>
2018-05-07 12:24:34 +0800

    gas estimating bug

--- Sign&deployContract.js
+++ Sign&deployContract.js
@@ -18,12 +18,12 @@ const private_key0 = '0x4fdeceafa4352e5eef12a4d34bf5bc9459c85462f7d5ebe59e6555ff
 
 //console.log(firstAccount.address);
 
-var tx = {data: '0x' + bytecode, gas: 1500000};
-/*var result = web3.eth.estimateGas({
-    to: '0x0',
+var tx = {data: '0x' + bytecode, gas: 4550000};
+web3.eth.estimateGas({
+    to: '0x0000000000000000000000000000000000000000',
     data: '0x' + bytecode
-});*/
-var result = web3.eth.estimateGas();
+}).then(console.log);
+//var result = web3.eth.estimateGas();
 console.log(result);
 //var signed = web3.eth.accounts.signTransaction(tx, private_key0).then(console.log);
 //web3.eth.sendSignedTransaction(signed.rawTransaction).then(console.log);
